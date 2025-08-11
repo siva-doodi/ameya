d1de9d35c21e1e335d8d3ef7560eadfffc75ccce
sivadoodi <doodisiva5@gmail.com>
2025-08-11 14:05:45 +0530

    Configure next.config.js for static export deployment

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  // optional basePath if you deploy to github pages under a subpath
+  // basePath: '/ameya',
+  // assetPrefix: '/ameya/',
+}
+
+module.exports = nextConfig
