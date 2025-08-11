421d7cbbd5d4ae4a5d4701a3b6c6ce7500153bee
sivadoodi <doodisiva5@gmail.com>
2025-08-11 14:48:14 +0530

    home page complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import AiHeroSection from "./components/home/AiHeroSection ";
+import AiManiFesto from "./components/home/AiManiFesto";
+import AiServiceSection from "./components/home/AiServiceSection";
+import AiContact from "./components/home/AiContact";
 
 export default function Home() {
   return (
     <>
       <AiHeroSection />
+      <AiManiFesto />
+      <AiServiceSection />
+      <AiContact />
     </>
   );
 }
