e2b38c97c9d7f7efa7d2afe62fffe9a9c7c6331b
sivadoodi <doodisiva5@gmail.com>
2025-08-11 14:33:39 +0530

    Your commit message here

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
   // optional basePath if you deploy to github pages under a subpath
   // basePath: '/ameya',
   // assetPrefix: '/ameya/',
